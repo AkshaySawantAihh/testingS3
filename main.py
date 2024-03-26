62c1858d19b87c26cbd39dd3bdc84a4bb34d0113
AkshaySawantAihh <164988820+AkshaySawantAihh@users.noreply.github.com>
2024-03-26 17:57:44 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import boto3
+
+# Create a Boto3 S3 client
+s3_client = boto3.client('s3')
+
+# Specify the bucket and object key
+bucket_name = 'avs-sagemaker-test-bucket'
+object_key = 'data/mobile_price_classification/test-V-1.csv'
+
+# Download the object from S3
+s3_client.download_file(bucket_name, object_key, 'test-V-1.csv')
+print("File downloaded successfully.")
+import pandas 
+print(df.read_csv("test-V-1.csv"))
